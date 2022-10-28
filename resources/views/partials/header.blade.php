f3ab056ac8b3d9a8c7bdb0ebfcc41f0bbfc14e61
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-10-28 16:23:56 +0200

    create header and footer

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="">CHARACTERS</a></li>
+            <li><a href="">COMICS</a></li>
+            <li><a href="">MOVIES</a></li>
+            <li><a href="">TV</a></li>
+            <li><a href="">GAMES</a></li>
+            <li><a href="">COLLECTIBLES</a></li>
+            <li><a href="">VIDEOS</a></li>
+            <li><a href="">FANS</a></li>
+            <li><a href="">NEWS</a></li>
+            <li><a href="">SHOP</a></li>
+        </ul>
+    </nav>
+    <div>
+        <img src="{{ asset('images/jumbotron.jpg') }}" alt="jumbotron">
+    </div>
+</header>
