423efed8f2dbf3fc286bb47e44885cec5117702c
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-10-28 18:25:36 +0200

    add images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
 @section('nomePerIlCollegamento')
-    holaaaaaaaaaaaaaaaaaaaaa
+    <div class="container">
+        <div class="copertine">
+            @foreach ($comics as $item => $formato)
+                <div class="card">
+                    <img src="{{ $formato['thumb'] }}" alt="{{ $formato['title'] }}">
+                    <h2>
+                        {{ $formato['title'] }}
+                    </h2>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $comics = config('data.comics');
+    $dati = [
+        'comics' => $comics
+    ];
+    return view('home', $dati);
 });
 
 Route::get('/fumetto', function () {
