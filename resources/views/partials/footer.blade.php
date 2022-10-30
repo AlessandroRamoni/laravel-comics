1df87e2dbc68d92e5be5ebaccf04ef69b61b6e97
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-10-30 10:35:47 +0100

    create footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="container-footer">
-        <img src="{{ asset('images/footer-bg.jpg') }}" alt="jumbotron">
+        {{-- <img src="{{ asset('images/footer-bg.jpg') }}" alt="jumbotron"> --}}
         <div class="container-elenchi">
             <div id="left-ul">
                 <ul>
@@ -46,6 +46,24 @@
                 </ul>
             </div>
         </div>
+        <div class="logo-footer">
+            <img src="{{ asset('images/dc-logo-bg.png') }}" alt="big logo">
+        </div>
     </div>
+    <div class="sfondo-basso-footer">
+        <div class="contenitore-icone">
+            <div><button>SIGN-UP NOW!</button></div>
+            <div id="social">
+                <div>FOLLOW US</div>
+                <div>
+                    <img src="{{ asset('images/footer-facebook.png') }}" alt="">
+                    <img src="{{ asset('images/footer-twitter.png') }}" alt="">
+                    <img src="{{ asset('images/footer-youtube.png') }}" alt="">
+                    <img src="{{ asset('images/footer-pinterest.png') }}" alt="">
+                    <img src="{{ asset('images/footer-periscope.png') }}" alt="">
 
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
