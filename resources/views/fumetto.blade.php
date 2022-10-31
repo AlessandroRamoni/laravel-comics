fdf1aaeb04c79cc18dffd6ebd77c3ed432914466
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-10-31 11:07:08 +0100

    create single page

---
+++ resources/views/fumetto.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Fumetto</title>
+</head>
+
+<body>
+
+    @include('partials.header')
+
+    @include('partials.banner')
+
+    @include('partials.footer')
+
+</body>
+
+</html>
